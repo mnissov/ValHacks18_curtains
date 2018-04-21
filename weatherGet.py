c91edfc0821dd58b935cece23b90437eb5b0d9c9
Unknown <s163962@student.dtu.dk>
2018-04-21 15:16:10 +0200

    initial test

---
+++ weatherGet.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 21 14:40:49 2018
+
+@author: morten
+"""
+
+from weather import Weather, Unit
+weather = Weather(unit=Unit.CELSIUS)
+
+# Lookup WOEID via http://weather.yahoo.com.
+
+lookup = weather.lookup(22126215)
+condition = lookup.condition
+print(condition.text)
+
+# Get weather forecasts for the upcoming days.
+
+forecasts = location.forecast
+for forecast in forecasts:
+    print(forecast.text)
+    print(forecast.date)
+    print(forecast.high)
+    print(forecast.low)
